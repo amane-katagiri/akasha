bc4ffbf866b9055426fd68878cc3283cea5b8689
Amane Katagiri <amane@ama.ne.jp>
2023-09-20 22:21:00 +0900

    Update schema generator

--- generate-schema.sh
+++ generate-schema.sh
@@ -1,3 +1,3 @@
-#!/bin/bash -Ceu
-curl https://api.openbd.jp/v1/schema > ./src/schema/openbd.json
+#!/bin/bash -eu
+curl https://api.openbd.jp/v1/schema | jq > ./src/schema/openbd.json
 npx json-schema-to-zod -s ./src/schema/openbd.json -t ./src/schema/openbd.ts
